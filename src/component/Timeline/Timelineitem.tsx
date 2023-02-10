9ab7974a28fe30cfcdb7f88a036eced09e1d456d
wxj01295860 <wxjing46@163.com>
2023-02-10 10:34:21 +0800

    feat: timeline

--- src/component/Timeline/Timelineitem.tsx
+++ src/component/Timeline/Timelineitem.tsx
@@ -5,18 +5,31 @@ import classnames from 'classnames';
 interface  Timeline{
     children:any
     time?:string
+    color?:string
+    img?:string
 }
 
 function TimeItem(props:Timeline) {
-    const {children,time} = props
+    const {children,time,color='red',img=''} = props
+    let background;
+    if(img) {
+        background = `url(${img})`
+    }
+
   return (
     <div className={classnames({
         time_line_common:true
     })}>
         <div className={classnames({
             spot:true,
-            spot_com:true
-        })}></div>
+            spot_com:true,
+            noborder:background
+        })}
+        style={{
+            border: `2px solid ${color}`,
+            background,
+        }}
+        ></div>
         <div className={classnames({
             line:true,
             line_com:true
--- src/component/Timeline/main.tsx
+++ src/component/Timeline/main.tsx
@@ -1,13 +1,19 @@
 import React,{useState,useRef,useEffect} from 'react';
-import { Tab, TimeItem, Timeline} from '../index';
+import { Switch, TimeItem, Timeline} from '../index';
 import classnames from 'classnames';
 
 function Timemain(){
+    const [type,setType] = useState('btn') as any
+    const changes = (flag:boolean) =>{
+        console.log(flag);
+        setType(flag ? 'center' : 'left')
+    }
   return  <div style={{'width':'300px'}}>
-    <Timeline line_left = 'center'>
-        <TimeItem time='2022年一月一日'>6月30日截止申请 网易开放暴雪游戏退款申请通道</TimeItem>
-        <TimeItem time='2022年一月一日'>网易：暴雪提议蛮横，不符合商业逻辑</TimeItem>
-        <TimeItem time='2022年一月一日'>暴雪中国：正在寻找机遇将游戏重新带回给大家</TimeItem>
+    <Switch switchChanges ={changes} text={false} left='center' right='left'></Switch>
+    <Timeline line_left = {type}>
+        <TimeItem time='2022年一月一日' color='pink'>6月30日截止申请 网易开放暴雪游戏退款申请通道6月30日截止申请</TimeItem>
+        <TimeItem time='2022年一月一日' color='yellow'>网易：暴雪提议蛮横，不符合商业逻辑网易：雪提议蛮横，不符合商业逻辑</TimeItem>
+        <TimeItem time='2022年一月一日' >暴雪中国：正在寻找机遇将游戏重新带回给大家</TimeItem>
         <TimeItem time='2022年'>网易：暴雪提议蛮横，不符合商业逻辑</TimeItem>
     </Timeline>
     </div>
